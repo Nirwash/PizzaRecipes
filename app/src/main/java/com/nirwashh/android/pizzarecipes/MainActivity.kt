86b8c1466ba011a3941d7e5d1110d5c131eaae93
Nirwash <nirwash@bk.ru>
2022-04-27 20:45:01 +0300

    Initial commit создание MainFragment создание разметки item для RecyclerView

---
+++ app/src/main/java/com/nirwashh/android/pizzarecipes/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nirwashh.android.pizzarecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fragment = MainFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+    }
+}
---
+++ app/src/main/java/com/nirwashh/android/pizzarecipes/MainFragment.kt
@@ -0,0 +1,48 @@
+package com.nirwashh.android.pizzarecipes
+
+import android.content.Context
+import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.nirwashh.android.pizzarecipes.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    lateinit var b: FragmentMainBinding
+    lateinit var fm: FragmentManager
+    lateinit var list: ArrayList<List>
+    lateinit var adapter: MyAdapter
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fm = (context as FragmentActivity).supportFragmentManager
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflater = TransitionInflater.from(requireContext())
+        exitTransition = inflater.inflateTransition(R.transition.slide_right)
+        enterTransition = inflater.inflateTransition(R.transition.slide_out_right)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        b = FragmentMainBinding.inflate(inflater, container, false)
+        return b.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
